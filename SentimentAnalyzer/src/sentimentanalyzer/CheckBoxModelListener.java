d4a2f410a4f0f47955a2a8b552615e8cb894db6b
Nikolin Ngjela <nikolin.ngjela@iceshpk.al>
2015-10-20 09:17:05 +0200

    Update CheckBoxModelListener

--- SentimentAnalyzer/src/sentimentanalyzer/CheckBoxModelListener.java
+++ SentimentAnalyzer/src/sentimentanalyzer/CheckBoxModelListener.java
@@ -1,9 +1,11 @@
+package sentimentanalyzer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sentimentanalyzer;
+
 
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
@@ -35,15 +37,19 @@ public class CheckBoxModelListener implements TableModelListener {
 
             Boolean checked = (Boolean) model.getValueAt(row, column);
             if (checked) {
-                System.out.println(word + " with " + score + " " +  ": " + true);
+                
+                System.out.println("The word " + "'" + word + "'" + " with score " + score + " " +  ":  is displayed in graphic" );
                 
                 try{
                     this.mainFormControls.plotDataToGraphs_forXYSeriesChart_WordsCount(word.toString(), row);
+                    // This is for the sentiment score plot
+                    this.mainFormControls.plotDataToGraphs_forXYSeriesChart_SentimentScore(word.toString(), row);
                 }catch(Exception ex){
                     System.out.println(ex.getMessage());
                 }
             }else{
-                System.out.println(word + " with " + score + " " +  ": " + false);
+                
+                 System.out.println("The word " + "'" + word + "'" + " with score " + score + " " +  ":  is removed in graphic" );
                 
                 //JOptionPane.showMessageDialog(null, columnName, "CName", JOptionPane.INFORMATION_MESSAGE);
                 
@@ -51,6 +57,8 @@ public class CheckBoxModelListener implements TableModelListener {
                     //EACH REMOVAL HAS POINT ALSO THE COLUMNT FROM WHERE TO REMOVE
                     if(columnName.equals("Plot")){
                         this.mainFormControls.removeDataFromGrpah_forXYSeriesChart_WordsCount(row, "words");
+                        //This is used to remove the sentiment score plot
+                        this.mainFormControls.removeDataFromGrpah_forXYSeriesChart_SentimentScore(row, "sentiment");
                     }
                 }catch(Exception ex){
                     System.out.println(ex.getMessage());
