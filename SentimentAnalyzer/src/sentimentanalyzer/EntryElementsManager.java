274ed3d5ac5abd29f331a51afae0d6bfc1eee028
Nikolin Ngjela <nikolin.ngjela@iceshpk.al>
2015-10-20 09:18:34 +0200

    Update EntryElementsManager

--- SentimentAnalyzer/src/sentimentanalyzer/EntryElementsManager.java
+++ SentimentAnalyzer/src/sentimentanalyzer/EntryElementsManager.java
@@ -1,13 +1,16 @@
+package sentimentanalyzer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sentimentanalyzer;
+
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
@@ -27,19 +30,22 @@ on: JsonParserT.java, on line: 157 - 159
 public class EntryElementsManager {
     
     public int interval;
-    public LocalDateTime from;
-    public LocalDateTime to;
+    public Date from;
+    public Date to;
     public int cicles = 0;
     
-    public LocalDateTime dtHelper_from;
-    public LocalDateTime dtHelper_to;
+    public Date dtHelper_from;
+    public Date dtHelper_to;
     
     public ArrayList<EntryElementsDate> wordsAndCreatedDates_ofManager;
     public ArrayList<EntryElementsHolder> lists;
     
     private LinkedList<Tweet> tweets;
     
-    public EntryElementsManager(int interval, LocalDateTime from, LocalDateTime to, ArrayList<EntryElementsDate> wordsAndCreatedDates){
+    private static final int MILLISECONDS_IN_A_MINUTE = 60000;
+    
+    
+    public EntryElementsManager(int interval, Date from, Date to, ArrayList<EntryElementsDate> wordsAndCreatedDates){
         this.interval = interval;
         this.from = from;
         this.to = to;
@@ -49,9 +55,9 @@ public class EntryElementsManager {
         this.wordsAndCreatedDates_ofManager = wordsAndCreatedDates;
         
         //JOptionPane.showMessageDialog(null, "FROM: " + from.toString() + "   TO: " + to.toString() + " Interval: " +  interval, "Data - EntryElementManager", JOptionPane.INFORMATION_MESSAGE);
-    }
+    } 
     
-    public EntryElementsManager(int interval, LocalDateTime from, LocalDateTime to, LinkedList<Tweet> tweets){
+    public EntryElementsManager(int interval, Date from, Date to, LinkedList<Tweet> tweets){
         this.interval = interval;
         this.from = from;
         this.to = to;
@@ -61,8 +67,14 @@ public class EntryElementsManager {
         
     }
     
-    public int[] getChartDataForWord(String word){
-        double difference = ChronoUnit.MINUTES.between(this.from, this.to);
+    public int[][] getChartDataForWord(String word){
+        
+        double difference = (this.to.getTime() - this.from.getTime())/ MILLISECONDS_IN_A_MINUTE;
+        
+        // Testing the difference from the interval date choosen.
+       // System.out.println(difference + "Difffffff");
+        
+        
         double differenceDivInterval = difference / (this.interval * 60);
         
         // Using the floor method 
@@ -70,41 +82,61 @@ public class EntryElementsManager {
         
         int valueToInitialize = (int)numberOfSlots;
         
-        int[] result = new int[valueToInitialize];
+        int[] result_wordCounter = new int[valueToInitialize];
+        int[] result_sentimentAvarage = new int[valueToInitialize];
         
         for (Tweet t : tweets){
             if (t.words.contains(word)){
-                double div2= ChronoUnit.MINUTES.between(this.from, t.createdAt);
+                double div2=(t.createdAt.getTime() - this.from.getTime())/ MILLISECONDS_IN_A_MINUTE;
+                
+                // We can output the first TOP WORD created at TIME with no loosing time
+                //System.out.println(div2 + "Difffffff22222" + "from: " + this.from.toString() + "created:" + t.createdAt.toString());
+                
                 double twwetdiff = div2 / (this.interval * 60);
-        
-        
+
                 double diff3 = Math.ceil(twwetdiff);
         
-                int slot = (int)diff3 ;
+                int slot = (int)diff3;
                 
-               
+                //THIS STORES THE TOTAL NUMBER OF TIMES A WORD IS FOUND
+                result_wordCounter[slot - 1] = result_wordCounter[slot - 1] + 1;
                 
-                result[slot - 1] = result[slot -1] + 1;
-           
-                //
-                
+                //ADDS TWEETS SENTIMENT SCORE IN TOTAL one + one + etc.
+                result_sentimentAvarage[slot - 1] = result_sentimentAvarage[slot - 1] + t.sentimetScore;
             }
         }
-        return result;
+        
+        int countCicle = 0;
+        for(int total : result_sentimentAvarage){
+            //System.out.println(total + "   is the value at index[" + countCicle + "]");
+            if(total != 0){
+                result_sentimentAvarage[countCicle] = total / result_wordCounter[countCicle];
+                //System.out.println("      operation performed is: " + total + "   divided with: " + result_wordCounter[countCicle] + "    produced: " + result_sentimentAvarage[countCicle]);
+            }
+            countCicle++;
+        }
+        
+        int[][] arraysToReturn = new int[][] {result_wordCounter, result_sentimentAvarage};
+        
+        return arraysToReturn;
     }
+    
+    
     
     public EntryElementsManager(){
         this.lists = new ArrayList<>();
         this.wordsAndCreatedDates_ofManager = new ArrayList<>();
-    }
+    } 
     
+    
+    /*
     //FUNCTION IN ORDER TO CREATE SMALL LISTS WITH INTERVAL TIME FRAMES
     public void prepareLists(){
         this.cicles = 0;
         int i = this.interval;
         
-        LocalDateTime localFrom = this.from;
-        LocalDateTime localTo = this.from; //this.from.plusHours(i);
+        Date localFrom = this.from;
+        Date localTo = this.from; //this.from.plusHours(i);
         //JOptionPane.showMessageDialog(null, "FROM: " + this.from, "The time you have selected...", JOptionPane.INFORMATION_MESSAGE);
         //JOptionPane.showMessageDialog(null, "FROM: " + this.to, "The time you have selected...", JOptionPane.INFORMATION_MESSAGE);
         
@@ -127,19 +159,25 @@ public class EntryElementsManager {
         }
         
         //JOptionPane.showMessageDialog(null, "A total of " + this.cicles + " cicles generated.", "Intervals", JOptionPane.INFORMATION_MESSAGE);
-    }
+    } 
     
+    */
+    
+    
+    
+     /*
     //POPULATES ALL THE TIMEFRAMES ACCORDINGLY WITH DATA FROM THE WORDS, STILL NOT COUNTED BUT INSIDE THE RIGHT DIVISION
     public void populateMyData(){
         for(int i=0; i<this.wordsAndCreatedDates_ofManager.size(); i++){
             for(int j=0; j<this.lists.size(); j++){
-                if((this.wordsAndCreatedDates_ofManager.get(i).date.compareTo(lists.get(j).from) >= 0) && (this.wordsAndCreatedDates_ofManager.get(i).date.compareTo(lists.get(j).to) <= 0)){
+                if((this.wordsAndCreatedDates_ofManager.get(i).date.after(lists.get(j).from)) && (this.wordsAndCreatedDates_ofManager.get(i).date.before(lists.get(j).to))){
                     this.lists.get(j).wordsAndCreatedDates_ofHolder.add(this.wordsAndCreatedDates_ofManager.get(i));
                 }
             }
         }
     }
     
+    // Used to caon the occurance for every hour and sort the word on each list
     //COUNT EACH WORD OCCURANCE FOR EACH INDIVIDUAL HOUR
     public void countAllWordsInTheLists(){
         for(int i=0; i<this.lists.size(); i++){
@@ -147,7 +185,7 @@ public class EntryElementsManager {
                 this.lists.get(i).counterForTheWords_individualObjectList();
             }
         }
-    }
+    } 
 
     //SORT THE WORD ON EACH LIST FROM TOP TO BOTTOM
     public void sortAllWordsInTheLists_afterCounted(){
@@ -156,7 +194,9 @@ public class EntryElementsManager {
                 this.lists.get(i).sortForTheWords_individualObjectList();
             }
         }
-    }
+    }  */
+    
+    
     
     //FUNCTION ONLY TO CHECK IF DATA ARE STORED CORRECTLY AND STATS
     public void checkIntegrityAndData(){
@@ -191,5 +231,3 @@ public class EntryElementsManager {
         }
     }
 }
-
-
